c9ca9a4773f551749e55399cd4900f4ac1af123c
Lucie Hutchins <LucyHut@users.noreply.github.com>
2017-07-24 10:22:26 -0400

    Update query.sql

--- archives/tools/transcriptdb/sql/query.sql
+++ archives/tools/transcriptdb/sql/query.sql
@@ -1,3 +1,12 @@
 use graber_transcriptdb;
 
-select distinct gene_prediction_name,sample_name,ta.transcript_name,ga.gene_name from rnaseq_sample s, rnaseq_transcript rt, gene_prediction p, transcript_by_annotation ta, gene_by_annotation ga where s.sample_id=rt.sample_id and rt.transcript_name=ta.transcript_name and rt.gene_prediction_id=ta.gene_prediction_id and rt.organism_version_id=ta.organism_version_id and ta.transcript_id=ga.transcript_id and ta.gene_prediction_id=ga.gene_prediction_id and ga.gene_prediction_id=p.gene_prediction_id and p.gene_prediction_id=rt.gene_prediction_id;
+select distinct gene_prediction_name,sample_name,ta.transcript_name,ga.gene_name 
+from rnaseq_sample s, rnaseq_transcript rt, gene_prediction p, transcript_by_annotation ta, gene_by_annotation ga 
+where s.sample_id=rt.sample_id 
+and rt.transcript_name=ta.transcript_name 
+and rt.gene_prediction_id=ta.gene_prediction_id 
+and rt.organism_version_id=ta.organism_version_id 
+and ta.transcript_id=ga.transcript_id 
+and ta.gene_prediction_id=ga.gene_prediction_id 
+and ga.gene_prediction_id=p.gene_prediction_id 
+and p.gene_prediction_id=rt.gene_prediction_id;
